7eb09fd632233b7593dac5ccb5b7f67bdaca1647
Viyath Araliya <viyatharaliya@gmail.com>
2025-03-01 05:12:33 +0530

    implmenet functionality to contain the particles in the window

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 const particles=[];
 const timesToMove1px_x=[]
 const timesToMove1px_y=[]
+const numberOfParticles=500;
+let xIncrements=[];
+let yIncrements=[];
+let xDirectionPositives=[];
+let yDirectionPositives=[];
+// let xIncrement=1;
+// let yIncrement=1;
 
 //initializeng particles
-for (let i=0;i<10;i++){
+for (let i=0;i<numberOfParticles;i++){
     const particle=document.createElement('div');
-    let length=20*(1+Math.random());
+    let length=5*(1+Math.random());
     particle.style.width=`${length}px`;
     particle.style.height=`${length}px`;
     particle.style.position='absolute';
@@ -15,6 +22,9 @@ for (let i=0;i<10;i++){
     particle.style.left=`${xPosition}px`;
     particle.style.top=`${yPostion}px`;
     particle.style.backgroundColor=`rgb(${225*Math.random()} ${225*Math.random()} ${225*Math.random()})`;
+
+    xIncrements.push(1);
+    yIncrements.push(1);
 
     let timeToMove1px_x=1+(20*Math.random());
     let timeToMove1px_y=1+(20*Math.random());
@@ -26,26 +36,49 @@ for (let i=0;i<10;i++){
     document.body.append(particle);
 }
 
-function moveParticle_x(particle,timeToMove1px_x){
+function moveParticle_x(particle,timeToMove1px_x,particleNumber){
+    xDirectionPositives[particleNumber]=true;
+
     setInterval(()=>{
-        particle.style.left=`${
-            parseInt(particle.style.left.substring(0,particle.style.left.length-2))+1}px`;
-        // particle.style.top=`${
-        //     parseInt(particle.style.top.substring(0,particle.style.top.length-2))+1}px`;
+        let particleLeft=particle.style.left.substring(0,particle.style.left.length-2);
+        particle.style.left=`${parseInt(particleLeft)+xIncrements[particleNumber]}px`;
+        if (innerWidth<=parseInt(particleLeft) && xDirectionPositives[particleNumber]){
+            xIncrements[particleNumber]=-xIncrements[particleNumber];
+            xDirectionPositives[particleNumber]=!xDirectionPositives[particleNumber];
+        }
+        if (parseInt(particleLeft)<=0 && !xDirectionPositives[particleNumber]){
+
+            xIncrements[particleNumber]=-xIncrements[particleNumber];
+            xDirectionPositives[particleNumber]=!xDirectionPositives[particleNumber];
+        }
+
     },timeToMove1px_x)
 }
 
 
-function moveParticle_y(particle,timeToMove1px_y){
+function moveParticle_y(particle,timeToMove1px_y,particleNumber){
+    yDirectionPositives[particleNumber]=true;
     setInterval(()=>{
-        particle.style.top=`${
-            parseInt(particle.style.top.substring(0,particle.style.top.length-2))+1}px`;
-        // particle.style.top=`${
-        //     parseInt(particle.style.top.substring(0,particle.style.top.length-2))+1}px`;
+        let particleTop=particle.style.top.substring(0,particle.style.top.length-2);
+        particle.style.top=`${parseInt(particleTop)+yIncrements[particleNumber]}px`;
+        if(innerHeight<=parseInt(particleTop) && yDirectionPositives[particleNumber]){
+            yIncrements[particleNumber]=-yIncrements[particleNumber]
+            yDirectionPositives[particleNumber]=!yDirectionPositives[particleNumber];
+        }
+
+        if (parseInt(particleTop)<=0 && !yDirectionPositives[particleNumber]){
+
+            yIncrements[particleNumber]=-yIncrements[particleNumber];
+            yDirectionPositives[particleNumber]=!yDirectionPositives[particleNumber];
+        }
+
+
     },timeToMove1px_y)
 }
-moveParticle_x(particles[0],timesToMove1px_x[0]);
-moveParticle_y(particles[0],timesToMove1px_y[0]);
 
-// 1/time
+//set movement for particles
+for (let i=0;i<numberOfParticles;i++){
+    moveParticle_x(particles[i],timesToMove1px_x[i],i);
+    moveParticle_y(particles[i],timesToMove1px_y[i],i);
+}
 
